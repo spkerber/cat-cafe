36e668f4e4afb850898852763eb6e7d72e2d7c8c
Spencer Kerber <spencer@spkerber.com>
2024-12-20 22:29:05 -0800

    Updating for basepath and assetprefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     images: {
       unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/cat-cafe' : '',
-  }
+    basePath: '/cat-cafe',
+    assetPrefix: '/cat-cafe',
+  };
   
-  module.exports = nextConfig
+  module.exports = nextConfig;
