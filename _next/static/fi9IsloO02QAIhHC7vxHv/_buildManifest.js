7cb2e0befbfcc9b1df005f21a540f7b438df4f7c
Spencer Kerber <spencer@spkerber.com>
2024-12-20 22:21:35 -0800

    Deploy static site to gh-pages

---
+++ _next/static/fi9IsloO02QAIhHC7vxHv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-a69cb561a1f949dd.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/schematic":["static/chunks/pages/schematic-cb5c85922f504d2d.js"],sortedPages:["/","/_app","/_error","/schematic"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
